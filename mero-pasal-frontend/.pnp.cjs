e1c9057a2f0aec28796c9a62c70e026ac44a1507
Yogesh <iamyogess@gmail.com>
2024-02-05 20:17:49 +0545

    commit

--- mero-pasal-frontend/.pnp.cjs
+++ mero-pasal-frontend/.pnp.cjs
@@ -31,6 +31,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/react", "npm:18.2.52"],\
             ["@types/react-dom", "npm:18.2.18"],\
             ["@vitejs/plugin-react", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:4.2.1"],\
+            ["axios", "npm:1.6.7"],\
             ["eslint", "npm:8.56.0"],\
             ["eslint-plugin-react", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:7.33.2"],\
             ["eslint-plugin-react-hooks", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:4.6.0"],\
@@ -39,6 +40,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-dom", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:18.2.0"],\
             ["react-icons", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:5.0.1"],\
             ["react-router-dom", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:6.22.0"],\
+            ["react-toastify", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:10.0.4"],\
             ["vite", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:5.0.12"]\
           ],\
           "linkType": "SOFT"\
@@ -1286,11 +1288,32 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["asynckit", [\
+        ["npm:0.4.0", {\
+          "packageLocation": "./.yarn/cache/asynckit-npm-0.4.0-c718858525-7b78c451df.zip/node_modules/asynckit/",\
+          "packageDependencies": [\
+            ["asynckit", "npm:0.4.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["available-typed-arrays", [\
         ["npm:1.0.6", {\
           "packageLocation": "./.yarn/cache/available-typed-arrays-npm-1.0.6-46d3afbf21-8295571eb8.zip/node_modules/available-typed-arrays/",\
           "packageDependencies": [\
             ["available-typed-arrays", "npm:1.0.6"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["axios", [\
+        ["npm:1.6.7", {\
+          "packageLocation": "./.yarn/cache/axios-npm-1.6.7-d7b9974d1b-87d4d42992.zip/node_modules/axios/",\
+          "packageDependencies": [\
+            ["axios", "npm:1.6.7"],\
+            ["follow-redirects", "virtual:d7b9974d1bba76881cc57a280a16dd4914416a6fc4923c2efbb6328057412974da1e719cef1530b7a62b97d85d828f7e1d49b5f6de3b5b0854d49902ec87827c#npm:1.15.5"],\
+            ["form-data", "npm:4.0.0"],\
+            ["proxy-from-env", "npm:1.1.0"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -1426,6 +1449,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["clsx", [\
+        ["npm:2.1.0", {\
+          "packageLocation": "./.yarn/cache/clsx-npm-2.1.0-29d286e1de-43fefc29b6.zip/node_modules/clsx/",\
+          "packageDependencies": [\
+            ["clsx", "npm:2.1.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["color-convert", [\
         ["npm:1.9.3", {\
           "packageLocation": "./.yarn/cache/color-convert-npm-1.9.3-1fe690075e-fd7a64a17c.zip/node_modules/color-convert/",\
@@ -1456,6 +1488,16 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/color-name-npm-1.1.4-025792b0ea-b044585952.zip/node_modules/color-name/",\
           "packageDependencies": [\
             ["color-name", "npm:1.1.4"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["combined-stream", [\
+        ["npm:1.0.8", {\
+          "packageLocation": "./.yarn/cache/combined-stream-npm-1.0.8-dc14d4a63a-49fa4aeb49.zip/node_modules/combined-stream/",\
+          "packageDependencies": [\
+            ["combined-stream", "npm:1.0.8"],\
+            ["delayed-stream", "npm:1.0.0"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -1551,6 +1593,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["define-data-property", "npm:1.1.1"],\
             ["has-property-descriptors", "npm:1.0.1"],\
             ["object-keys", "npm:1.1.1"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["delayed-stream", [\
+        ["npm:1.0.0", {\
+          "packageLocation": "./.yarn/cache/delayed-stream-npm-1.0.0-c5a4c4cc02-46fe6e83e2.zip/node_modules/delayed-stream/",\
+          "packageDependencies": [\
+            ["delayed-stream", "npm:1.0.0"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -2084,6 +2135,28 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["follow-redirects", [\
+        ["npm:1.15.5", {\
+          "packageLocation": "./.yarn/cache/follow-redirects-npm-1.15.5-9d14db76ca-5ca49b5ce6.zip/node_modules/follow-redirects/",\
+          "packageDependencies": [\
+            ["follow-redirects", "npm:1.15.5"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:d7b9974d1bba76881cc57a280a16dd4914416a6fc4923c2efbb6328057412974da1e719cef1530b7a62b97d85d828f7e1d49b5f6de3b5b0854d49902ec87827c#npm:1.15.5", {\
+          "packageLocation": "./.yarn/__virtual__/follow-redirects-virtual-393395f3f6/0/cache/follow-redirects-npm-1.15.5-9d14db76ca-5ca49b5ce6.zip/node_modules/follow-redirects/",\
+          "packageDependencies": [\
+            ["follow-redirects", "virtual:d7b9974d1bba76881cc57a280a16dd4914416a6fc4923c2efbb6328057412974da1e719cef1530b7a62b97d85d828f7e1d49b5f6de3b5b0854d49902ec87827c#npm:1.15.5"],\
+            ["@types/debug", null],\
+            ["debug", null]\
+          ],\
+          "packagePeers": [\
+            "@types/debug",\
+            "debug"\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["for-each", [\
         ["npm:0.3.3", {\
           "packageLocation": "./.yarn/cache/for-each-npm-0.3.3-0010ca8cdd-6c48ff2bc6.zip/node_modules/for-each/",\
@@ -2101,6 +2174,18 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["foreground-child", "npm:3.1.1"],\
             ["cross-spawn", "npm:7.0.3"],\
             ["signal-exit", "npm:4.1.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["form-data", [\
+        ["npm:4.0.0", {\
+          "packageLocation": "./.yarn/cache/form-data-npm-4.0.0-916facec2d-01135bf867.zip/node_modules/form-data/",\
+          "packageDependencies": [\
+            ["form-data", "npm:4.0.0"],\
+            ["asynckit", "npm:0.4.0"],\
+            ["combined-stream", "npm:1.0.8"],\
+            ["mime-types", "npm:2.1.35"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -2976,6 +3061,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/react", "npm:18.2.52"],\
             ["@types/react-dom", "npm:18.2.18"],\
             ["@vitejs/plugin-react", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:4.2.1"],\
+            ["axios", "npm:1.6.7"],\
             ["eslint", "npm:8.56.0"],\
             ["eslint-plugin-react", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:7.33.2"],\
             ["eslint-plugin-react-hooks", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:4.6.0"],\
@@ -2984,9 +3070,29 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react-dom", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:18.2.0"],\
             ["react-icons", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:5.0.1"],\
             ["react-router-dom", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:6.22.0"],\
+            ["react-toastify", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:10.0.4"],\
             ["vite", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:5.0.12"]\
           ],\
           "linkType": "SOFT"\
+        }]\
+      ]],\
+      ["mime-db", [\
+        ["npm:1.52.0", {\
+          "packageLocation": "./.yarn/cache/mime-db-npm-1.52.0-b5371d6fd2-0d99a03585.zip/node_modules/mime-db/",\
+          "packageDependencies": [\
+            ["mime-db", "npm:1.52.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["mime-types", [\
+        ["npm:2.1.35", {\
+          "packageLocation": "./.yarn/cache/mime-types-npm-2.1.35-dd9ea9f3e2-89a5b7f1de.zip/node_modules/mime-types/",\
+          "packageDependencies": [\
+            ["mime-types", "npm:2.1.35"],\
+            ["mime-db", "npm:1.52.0"]\
+          ],\
+          "linkType": "HARD"\
         }]\
       ]],\
       ["minimatch", [\
@@ -3439,6 +3545,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["proxy-from-env", [\
+        ["npm:1.1.0", {\
+          "packageLocation": "./.yarn/cache/proxy-from-env-npm-1.1.0-c13d07f26b-ed7fcc2ba0.zip/node_modules/proxy-from-env/",\
+          "packageDependencies": [\
+            ["proxy-from-env", "npm:1.1.0"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["punycode", [\
         ["npm:2.3.1", {\
           "packageLocation": "./.yarn/cache/punycode-npm-2.3.1-97543c420d-bb0a0ceedc.zip/node_modules/punycode/",\
@@ -3572,6 +3687,33 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:18.2.0"],\
             ["react-router", "virtual:572228c51537bdd25a2be3087e97111a592882011f2c7d2bbff088cec329c9d808edcad0318b0fd36e42206898628623ece24bbf37f5b251228287df94fa690f#npm:6.22.0"]\
+          ],\
+          "packagePeers": [\
+            "@types/react-dom",\
+            "@types/react",\
+            "react-dom",\
+            "react"\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
+      ["react-toastify", [\
+        ["npm:10.0.4", {\
+          "packageLocation": "./.yarn/cache/react-toastify-npm-10.0.4-e28f40a449-fea1b6924f.zip/node_modules/react-toastify/",\
+          "packageDependencies": [\
+            ["react-toastify", "npm:10.0.4"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:10.0.4", {\
+          "packageLocation": "./.yarn/__virtual__/react-toastify-virtual-9e1d0db1fc/0/cache/react-toastify-npm-10.0.4-e28f40a449-fea1b6924f.zip/node_modules/react-toastify/",\
+          "packageDependencies": [\
+            ["react-toastify", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:10.0.4"],\
+            ["@types/react", "npm:18.2.52"],\
+            ["@types/react-dom", "npm:18.2.18"],\
+            ["clsx", "npm:2.1.0"],\
+            ["react", "npm:18.2.0"],\
+            ["react-dom", "virtual:dc473d0658f138cdd56a406d81aa37504f02091640131870b3d9229505e30f698abc2c29b3ee128d25d4cffc8250a95d6422ec3dc47b309ec8a1c19eff84ee61#npm:18.2.0"]\
           ],\
           "packagePeers": [\
             "@types/react-dom",\
--- mero-pasal-frontend/src/pages/Auth/Register.jsx
+++ mero-pasal-frontend/src/pages/Auth/Register.jsx
@@ -12,42 +12,53 @@ const Register = () => {
     password: ""
   })
 
+  const handleRegister = (e) => {
+    const { name, value } = e.target;
+    setRegisterData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(registerData)
+    console.log("hello")
+  }
+
   return (
     <Layout>
       <div className="d-flex align-items-center justify-content-center py-4">
         <div className="card p-4" style={{ maxWidth: '600px', width: '90%' }}>
           <h2 className="text-center mb-4">Register</h2>
-          <form>
+          <form  onSubmit={handleSubmit}>
 
             <div className="row mb-3">
               <div className="col">
                 <label htmlFor="firstName" className="form-label">First Name</label>
-                <input type="text" id="firstName" className="form-control" name='firstName' />
+                <input type="text" id="firstName" className="form-control" name='firstName' onChange={handleRegister} required/>
               </div>
               <div className="col">
                 <label htmlFor="lastName" className="form-label">Last Name</label>
-                <input type="text" id="lastName" className="form-control" name='lastName' />
+                <input type="text" id="lastName" className="form-control" name='lastName' onChange={handleRegister} required/>
               </div>
             </div>
 
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Email address</label>
-              <input type="email" id="email" className="form-control" name='email' />
+              <input type="email" id="email" className="form-control" name='email' onChange={handleRegister} required/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
-              <input type="tel" id="phoneNumber" className="form-control" name='phone' />
+              <input type="tel" id="phoneNumber" className="form-control" name='phone' onChange={handleRegister} required/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="address" className="form-label">Address</label>
-              <input type="text" id="address" className="form-control" name='address' />
+              <input type="text" id="address" className="form-control" name='address' onChange={handleRegister} required/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" id="password" className="form-control" name='password' />
+              <input type="password" id="password" className="form-control" name='password' onChange={handleRegister} required/>
             </div>
             {/* 
             <div className="mb-3">
@@ -61,7 +72,7 @@ const Register = () => {
             </div>
 
             <div className="d-grid gap-2">
-              <button type="button" className="btn btn-primary btn-block">Register</button>
+              <button type="submit" className="btn btn-primary btn-block">Register</button>
             </div>
 
             <div className="text-center mt-3">
