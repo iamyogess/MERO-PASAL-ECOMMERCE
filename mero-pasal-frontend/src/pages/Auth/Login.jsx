d11a4fb69a7c09b8381261199179b45c6b229af0
Yogesh <iamyogess@gmail.com>
2024-02-06 12:19:58 +0545

    login functionality

--- mero-pasal-frontend/src/pages/Auth/Login.jsx
+++ mero-pasal-frontend/src/pages/Auth/Login.jsx
@@ -1,45 +1,71 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Layout from '../../components/Layout/Layout';
-import  axios  from 'axios';
-import toast from 'react-hot-toast';
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Login = () => {
   const navigate = useNavigate();
 
   const [loginData, setLoginData] = useState({
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
   const handleLogin = (e) => {
     const { name, value } = e.target;
-    setLoginData((prev) => ({ ...prev, [name]: value }))
-  }
+    setLoginData((prev) => ({ ...prev, [name]: value }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await axios.post("http://localhost:8000/api/v1/auth/login/",loginData);
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/api/v1/auth/login/",
+        loginData
+      );
+      if (res && res.data.success) {
+        toast.success(res.data.message);
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong!");
+    }
+  };
 
-  }
-  
   return (
     <Layout>
-      
       <div className="d-flex align-items-center justify-content-center vh-100">
-        <div className="card p-4" style={{ maxWidth: '600px', width: '90%' }}>
+        <div className="card p-4" style={{ maxWidth: "600px", width: "90%" }}>
           <h2 className="text-center mb-4">Login</h2>
           <form onSubmit={handleSubmit}>
-
             <div className="mb-3">
-              <label htmlFor="email" className="form-label">Email address</label>
-              <input type="email" id="email" className="form-control" name='email' onChange={handleLogin}/>
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                name="email"
+                onChange={handleLogin}
+              />
             </div>
 
             <div className="mb-3">
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" id="password" className="form-control" name='password' onChange={handleLogin}/>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                name="password"
+                onChange={handleLogin}
+              />
             </div>
 
             {/* <div className="mb-3 form-check">
@@ -48,11 +74,15 @@ const Login = () => {
             </div> */}
 
             <div className="d-grid gap-2">
-              <button type="button" className="btn btn-primary btn-block">Login</button>
+              <button type="submit" className="btn btn-primary btn-block">
+                Login
+              </button>
             </div>
 
             <div className="text-center mt-3">
-              <p>New user? <Link to="/register">Register</Link></p>
+              <p>
+                New user? <Link to="/register">Register</Link>
+              </p>
               <Link to="#!">Forgot password?</Link>
             </div>
           </form>
@@ -60,6 +90,6 @@ const Login = () => {
       </div>
     </Layout>
   );
-}
+};
 
 export default Login;
