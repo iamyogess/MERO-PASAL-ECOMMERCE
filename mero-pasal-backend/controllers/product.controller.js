e2c31c37bcc70c82a17e1743b0646e5ab41d16df
Yogesh <iamyogess@gmail.com>
2024-02-21 12:49:12 +0545

    product update

--- mero-pasal-backend/controllers/product.controller.js
+++ mero-pasal-backend/controllers/product.controller.js
@@ -1,100 +1,55 @@
-// import { ProductModel } from "../models/product.model.js";
-// import fs from "fs";
-// import slugify from "slugify";
-
-// export const createProductController = async (req, res) => {
-//   try {
-//     const { name, slug, description, price, category, quantity, shipping } =
-//       // req.fields;
-//       req.body;
-//     // const { photo } = req.files;
-
-//     if (!name) {
-//       return res.status(500).json({ error: "name is required!" });
-//     }
-//     if (!description) {
-//       return res.status(500).json({ error: "description is required!" });
-//     }
-//     if (!price) {
-//       return res.status(500).json({ error: "price is required!" });
-//     }
-//     if (!category) {
-//       return res.status(500).json({ error: "category is required!" });
-//     }
-//     if (!quantity) {
-//       return res.status(500).json({ error: "quantity is required!" });
-//     }
-//     // if (!photo) {
-//     //     return res.status(500).json({ error: "photo is required!" });
-//     // }
-//     // if (photo.size > 1000000) {
-//     //     return res.status(500).json({ error: "photo should be less than 1MB!" });
-//     // }
-
-//     // if (photo) {
-//     //     products.photo.data = fs.readFileSync(photo.path);
-//     //     products.photo.contentType = photo.type;
-//     // }
-
-//     const products = new ProductModel({ ...req.body, slug: slugify(name) });
-//     await products.save();
-//     res
-//       .status(200)
-//       .json({ success: true, message: "Product created!", products });
-//   } catch (error) {
-//     console.log(error);
-//     res
-//       .status(500)
-//       .json({ success: false, message: "Error in creating product!", error });
-//   }
-// };
-
 import { ProductModel } from "../models/product.model.js";
+import fs from "fs";
 import slugify from "slugify";
 
 export const createProductController = async (req, res) => {
   try {
-    // Destructure required fields from req.body directly
-    const { name, description, price, category, quantity, shipping } = req.body;
+    const { name, slug, description, price, category, quantity, shipping } =
+      // req.fields;
+      req.body;
+    // const { photo } = req.files;
 
-    // Check if required fields are present
-    if (!name || !description || !price || !category || !quantity) {
-      return res.status(400).json({ error: "All fields are required!" });
+    if (!name) {
+      return res.status(500).json({ error: "name is required!" });
     }
+    if (!description) {
+      return res.status(500).json({ error: "description is required!" });
+    }
+    if (!price) {
+      return res.status(500).json({ error: "price is required!" });
+    }
+    if (!category) {
+      return res.status(500).json({ error: "category is required!" });
+    }
+    if (!quantity) {
+      return res.status(500).json({ error: "quantity is required!" });
+    }
+    // if (!photo) {
+    //     return res.status(500).json({ error: "photo is required!" });
+    // }
+    // if (photo.size > 1000000) {
+    //     return res.status(500).json({ error: "photo should be less than 1MB!" });
+    // }
 
-    // Additional validation for fields can be added here if necessary
+    // if (photo) {
+    //     products.photo.data = fs.readFileSync(photo.path);
+    //     products.photo.contentType = photo.type;
+    // }
 
-    // Generate slug from name using slugify
-    const slug = slugify(name);
-
-    // Create a new product instance
-    const product = new ProductModel({
-      name,
-      description,
-      price,
-      category,
-      quantity,
-      slug, // Add slug to the product
-    });
-
-    // Save the product to the database
-    await product.save();
-
-    // Respond with success message and the created product
-    return res
-      .status(201)
-      .json({ success: true, message: "Product created!", product });
+    const products = new ProductModel({ ...req.body, slug: slugify(name) });
+    await products.save();
+    res
+      .status(200)
+      .json({ success: true, message: "Product created!", products });
   } catch (error) {
-    console.error("Error in creating product:", error);
-    // If an error occurs, respond with an error message
-    return res.status(500).json({
-      success: false,
-      message: "Error in creating product!",
-      error: error.message,
-    });
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error in creating product!", error });
   }
 };
 
+// get products
 export const getProductController = async (req, res) => {
   try {
     const products = await ProductModel.find({})
@@ -117,6 +72,7 @@ export const getProductController = async (req, res) => {
   }
 };
 
+//get single product
 export const getSingleProductController = async (req, res) => {
   try {
     const product = await ProductModel.findOne({ slug: req.params.slug })
@@ -134,6 +90,7 @@ export const getSingleProductController = async (req, res) => {
   }
 };
 
+//photo
 export const productPhotoController = async (req, res) => {
   try {
     const product = await ProductModel.findById(req.params.pid).select("photo");
@@ -148,5 +105,72 @@ export const productPhotoController = async (req, res) => {
     res
       .status(500)
       .json({ success: false, message: "Error while getting photo!", error });
+  }
+};
+
+//delete product
+export const deleteProductController = async (req, res) => {
+  try {
+    await ProductModel.findByIdAndDelete(req.params.pid).select("-photo");
+    return res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully!" });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error while deleting product!",
+      error,
+    });
+  }
+};
+
+export const updateProductController = async (req, res) => {
+  try {
+    const { name, slug, description, price, category, quantity, shipping } =
+      // req.fields;
+      req.body;
+    // const { photo } = req.files;
+
+    if (!name) {
+      return res.status(500).json({ error: "name is required!" });
+    }
+    if (!description) {
+      return res.status(500).json({ error: "description is required!" });
+    }
+    if (!price) {
+      return res.status(500).json({ error: "price is required!" });
+    }
+    if (!category) {
+      return res.status(500).json({ error: "category is required!" });
+    }
+    if (!quantity) {
+      return res.status(500).json({ error: "quantity is required!" });
+    }
+    // if (!photo) {
+    //     return res.status(500).json({ error: "photo is required!" });
+    // }
+    // if (photo.size > 1000000) {
+    //     return res.status(500).json({ error: "photo should be less than 1MB!" });
+    // }
+
+    // if (photo) {
+    //     products.photo.data = fs.readFileSync(photo.path);
+    //     products.photo.contentType = photo.type;
+    // }
+
+    const products = new ProductModel.findByIdAndUpdate(
+      req.params.pid,
+      { ...req.body, slug: slugify(name) },
+      { new: true }
+    );
+    await products.save();
+    res
+      .status(200)
+      .json({ success: true, message: "Product updated!", products });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error in updating product!", error });
   }
 };
--- mero-pasal-backend/routes/product.route.js
+++ mero-pasal-backend/routes/product.route.js
@@ -2,7 +2,14 @@ import express from "express";
 import formidable from "express-formidable";
 
 import { isAdmin, requireSignIn } from "./../middlewares/auth.middleware.js";
-import { createProductController,getProductController,getSingleProductController ,productPhotoController} from "../controllers/product.controller.js";
+import {
+  createProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  deleteProductController,
+  updateProductController
+} from "../controllers/product.controller.js";
 
 const router = express.Router();
 
@@ -16,10 +23,20 @@ router.post(
   createProductController
 );
 
-router.get("/get-product",getProductController);
+router.post(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
 
-router.get("get-product/:slug",getSingleProductController);
+router.get("/get-product", getProductController);
 
-router.get("/product-photo/:pid",productPhotoController)
+router.get("get-product/:slug", getSingleProductController);
+
+router.get("/product-photo/:pid", productPhotoController);
+
+router.delete("/product/:pid", deleteProductController);
 
 export default router;
